85444205e8412bde3583896fc12764cebdf34623
Abhilash Tiwari <send.to.abhilash@gmail.com>
2020-09-15 04:31:21 -0400

    Updating app name to knowall.notetub.com in protected/config/main.php, to separate notes of knowall, from others.

--- protected/config/main.php
+++ protected/config/main.php
@@ -9,7 +9,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Ava.NoteTub.com',
+	'name'=>'knowall.NoteTub.com',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
